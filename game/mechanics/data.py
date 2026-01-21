60a3847229b89ffe5633e3c703ae798e849b4643
ArtemLevVv <arzona.art@gmail.com>
2026-01-21 18:35:23 +0100

    add initial funktion and initial data for world

--- game/mechanics/data.py
+++ game/mechanics/data.py
@@ -1,1 +1,36 @@
-bottom = {} 
+import random, json, os
+
+# 0 = air, 1 = tree
+AIR = 0
+TREE = 1
+
+CHUNK_SIZE = 8  # 8x8 blocks
+
+world = {
+    "chunk00": [],
+    "chunk10": [],
+    "chunk01": [],
+    "chunk11": [],
+}
+
+FILE_PATH_WORLD = f'{__file__}/../../data/data.json'
+
+def random_chunk():
+    return [
+        [random.randint(AIR, TREE) for _ in range(CHUNK_SIZE)]
+        for _ in range(CHUNK_SIZE)
+    ]
+
+def init_data():
+    if not os.path.exists(FILE_PATH_WORLD):
+        for chunk_key in world:
+            print(f"Generating {chunk_key}")
+            world[chunk_key] = random_chunk()
+        with open(FILE_PATH_WORLD, 'w', encoding='utf-8') as file:
+            json.dump(world, file, ensure_ascii=False, indent=4)
+        print('create initial world file')
+    else:
+        print('initial file was created before')
+        
+# --- INIT ---
+init_data()
